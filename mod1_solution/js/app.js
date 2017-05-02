afcdd876ca9116faec67fbc81510ecb64fc94eb3
Eduardo <edufsh@gmail.com>
2017-05-02 09:14:51 -0300

    Adicionando os arquivos do professor

--- mod1_solution/js/app.js
+++ mod1_solution/js/app.js
@@ -1,5 +1,26 @@
-angular.module ('LunchCheck', []).controller('LunchCheckController', function() {
+var lunchApp = angular.module('LunchCheck', []);
 
-
+lunchApp.controller('LunchCheckController', ['$scope', function LunchCheckController($scope){
 	
-});
+	var vm = $scope;
+	
+	vm.message = "";
+	vm.check = checkItens;
+	
+	function checkItens(){
+		
+		count = vm.itens.split(",").length;
+		if (count > 3) {
+			
+			vm.message = "Too much!";
+			
+		} else {
+			
+			vm.message = "Enjoy!";
+			
+		}
+		
+	};
+	
+	
+}]);
