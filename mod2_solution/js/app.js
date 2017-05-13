a86955723b3cc5eba4595af77c9980bca473c3dc
Jaconé <edufsh@gmail.com>
2017-05-13 18:54:45 -0300

    PAsso 7

---
+++ mod2_solution/js/app.js
@@ -0,0 +1,18 @@
+(function(){
+	'use strict';
+
+	var lunchApp = angular.module('ShoppingListCheckOff', []);
+
+	lunchApp.controller('ToBuyController', ['$scope', function ToBuyController($scope){
+		
+		var vm = $scope;
+
+	}]);
+
+	lunchApp.controller('AlreadyBoughtController', ['$scope', function AlreadyBoughtController($scope){
+		
+		var vm = $scope;
+
+	}]);
+
+})();
