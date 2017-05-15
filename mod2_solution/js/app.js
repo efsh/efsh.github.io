bfd6f4c73d480d8e602d29de3c914764e8369a15
Jaconé <edufsh@gmail.com>
2017-05-15 05:53:09 -0300

    Criando o service

--- mod2_solution/js/app.js
+++ mod2_solution/js/app.js
@@ -1,18 +1,79 @@
 (function(){
 	'use strict';
 
-	var lunchApp = angular.module('ShoppingListCheckOff', []);
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyController', ToBuyController)
+	.controller('AlreadyBoughtController', AlreadyBoughtController)
+	.service('ListService', ListService);
 
-	lunchApp.controller('ToBuyController', ['$scope', function ToBuyController($scope){
+	ToBuyController.$inject = ['ListService'];
+	function ToBuyController(ListService){
 		
-		var vm = $scope;
+		var itemBuy = this;
 
-	}]);
+		itemBuy.addItem = function () {
+			ListService.addItem(itemBuy);
+		};
 
-	lunchApp.controller('AlreadyBoughtController', ['$scope', function AlreadyBoughtController($scope){
+		itemBuy.removeItem = function (itemIndex) {
+			ListService.removeItem(itemIndex);
+		};
+
+
+	};
+
+	AlreadyBoughtController.$inject = ['ListService'];
+	function AlreadyBoughtController(ListService){
 		
-		var vm = $scope;
+		var boughtItems = this;
 
-	}]);
+		boughtItems.items = ListService.getBoughtItems();
+
+	};
+
+	function ListService() {
+
+		var service = this;
+		var boughtItems = [];
+		var toBuyItems = [
+			{
+				name: 'Charge',
+				quantity: 1
+			},
+			{
+				name: 'KitKat',
+				quantity: 2
+			},
+			{
+				name: 'Pretigio',
+				quantity: 3
+			},
+			{
+				name: 'Suflair',
+				quantity: 5
+			},
+			{
+				name: 'Sensação',
+				quantity: 7
+			}
+		];
+
+		service.addItem = function(item) {
+			boughtItems.push(item);
+		};
+
+		service.removeItem = function (itemIdex) {
+			toBuyItems.splice(itemIdex, 1);
+		};
+
+		service.getToBuyItems = function() {
+			return toBuyItems;
+		};
+
+		service.getBoughtItems = function() {
+			return boughtItems;
+		};
+
+	}
 
 })();
