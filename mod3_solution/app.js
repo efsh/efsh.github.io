75bb7a56245c46725a644c6483a478f4f59e332e
Jaconé <edufsh@gmail.com>
2017-05-28 22:18:57 -0300

    Criando serviço HTTP

---
+++ mod3_solution/app.js
@@ -0,0 +1,36 @@
+(function (){
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService);
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var menu = this;
+
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+    promise.then(function (response) {
+      menu.categories = response.data;
+    })
+    .catch(function (error){
+      console.log("ERROR");
+    })
+
+  };
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+      var response = $http({
+        method: "GET",
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+      });
+
+      return response;
+    };
+  };
+});
